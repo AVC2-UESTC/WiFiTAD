6c3272a067d550d0f840b7aacaadd1762f479da0
Lzdjohn <978228869@qq.com>
2024-12-17 11:29:56 +0800

    Delete TALFi directory

--- TALFi/train_tools/SlideWindow.sh
+++
@@ -1,8 +0,0 @@
-echo "start training"
-CUDA_VISIBLE_DEVICES=$1,$2 python3 TALFi/utils/train_SL.py configs/SL.yaml
-
-echo "start detecting..."
-CUDA_VISIBLE_DEVICES=$1 python3 TALFi/utils/test_SL.py configs/SL.yaml
-
-echo "start eval..."
-python3 TALFi/utils/eval.py configs/SL.yaml
