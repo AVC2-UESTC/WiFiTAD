47f9ce7b3d2bb7c2953a651bc8ec5218ad4ee343
Lzdjohn <978228869@qq.com>
2024-12-23 18:18:48 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,9 @@
 from setuptools import setup, find_packages
-from torch.utils.cpp_extension import BuildExtension, CUDAExtension
 
 if __name__ == '__main__':
     setup(
         name='TAD',
         version='1.0',
-        packages=find_packages(exclude=('cache', 'output', 'dataset')),
-        cmdclass={
-            'build_ext': BuildExtension
-        }
+        packages=find_packages(exclude=('cache', 'output', 'dataset'))
     )
     
